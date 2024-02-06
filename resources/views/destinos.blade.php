144b7305aa5138dfeab2e59fd6e398b1a5e79604
DanielCalzadaRubio <144838406+DanielCalzadaRubio@users.noreply.github.com>
2024-02-06 20:33:08 +0100

    Finalització página web destinos

--- resources/views/destinos.blade.php
+++ resources/views/destinos.blade.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
 <html>
 
 <head>
     <style>
         .container {
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(300px, 2fr));
+            display: flex;
             gap: 1rem;
             padding: 1rem;
+            width: 100%;
         }
 
         .card {
             border: 1px solid #ccc;
             border-radius: 1rem;
             padding: 1rem;
-            transition: transform 0.3s ease-in-out;
+            transition: transform 0.3s ease;
+            width: 47.5%;
+            margin: 15px;
         }
 
         .card:hover {
-            transform: scale(1.05);
+            transform: scale(1.025);
+            border-color: black;
         }
 
         .details {
@@ -31,11 +33,31 @@
             max-height: 100vh;
         }
 
-        @media screen and (max-width: 600px) {
-            .container {
-                grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));
-            }
+        h2 {
+            font-size: 35px;
         }
+
+        p {
+            font-size: 25px;
+            
+        }
+
+        button {
+            border: 1px solid rgba(0, 0, 0, .9);
+            border-radius: 7px;
+            transform: scale(2);
+            margin-left: 40px;
+            margin-bottom: 15px;
+            background-color: #ffffff;
+            transition: background 1s ease, transform 1s ease;
+        }
+
+        button:hover {
+            background-color: #ff0040;
+            transform: scale(2.15);
+            cursor: pointer;
+        }
+
     </style>
 </head>
 
@@ -47,12 +69,12 @@
             <h2>Alemania</h2>
             <button onclick="this.nextElementSibling.style.display='block'">View Detail</button>
             <div class="details" style="display: none;">
-                <p>Descripción: Conocida por su eficiencia y su rica historia, Alemania ofrece una variedad de
+                    <strong><p>Descripción:</strong> Conocida por su eficiencia y su rica historia, Alemania ofrece una variedad de
                     experiencias, desde explorar castillos antiguos y degustar cervezas locales hasta recorrer vibrantes
                     ciudades modernas como Berlín y Múnich.</p>
-                <p>Duración: 7 días</p>
-                <p>Mejor época: Mayo - Septiembre</p>
-                <p>Atracciones: Berlín, Múnich, Castillo de Neuschwanstein</p>
+                    <strong><p>Duración:</strong> 7 días</p>
+                    <strong><p>Mejor época:</strong> Mayo - Septiembre</p>
+            <strong><p>Atracciones:</strong> Berlín, Múnich, Castillo de Neuschwanstein</p>
             </div>
         </div>
         <div class="card">
@@ -60,27 +82,29 @@
             <h2>Argentina</h2>
             <button onclick="this.nextElementSibling.style.display='block'">View Detail</button>
             <div class="details" style="display: none;">
-                <p>Descripción: Argentina es conocida por su apasionante cultura del tango, su sabrosa carne de res y su
+            <strong><p>Descripción:</strong> Argentina es conocida por su apasionante cultura del tango, su sabrosa carne de res y su
                     amor por el fútbol. El país cuenta con una variedad de paisajes, desde las cataratas del Iguazú en
                     el norte hasta los glaciares de la Patagonia en el sur.</p>
-                <p>Duración: 14 días</p>
-                <p>Mejor época: Octubre - Abril</p>
-                <p>Atracciones: Buenos Aires, Cataratas del Iguazú, Glaciar Perito Moreno</p>
+                <strong><p>Duración:</strong> 14 días</p>
+                <strong><p>Mejor época:</strong> Octubre - Abril</p>
+                <strong><p>Atracciones:</strong> Buenos Aires, Cataratas del Iguazú, Glaciar Perito Moreno</p>
             </div>
         </div>
+    </div>
+    <div class="container">
         <div class="card">
             <img src="https://www.talesofawanderer.com/blog/wp-content/plugins/post-country/Flags/ESPANA.gif"
                 alt="Imagen de España">
             <h2>España</h2>
             <button onclick="this.nextElementSibling.style.display='block'">View Detail</button>
             <div class="details" style="display: none;">
-                <p>Descripción: Famosa por su diversidad cultural y geográfica, España ofrece todo, desde playas
+            <strong><p>Descripción:</strong> Famosa por su diversidad cultural y geográfica, España ofrece todo, desde playas
                     soleadas y montañas escarpadas hasta vibrantes ciudades y festivales coloridos. Las ciudades más
                     populares incluyen Barcelona, conocida por su arquitectura modernista, y Madrid, la capital, famosa
                     por su vida nocturna y sus museos de arte de clase mundial.</p>
-                <p>Duración: 10 días</p>
-                <p>Mejor época: Abril - Junio</p>
-                <p>Atracciones: Barcelona, Madrid, Sevilla</p>
+                <strong><p>Duración:</strong> 10 días</p>
+                <strong><p>Mejor época:</strong> Abril - Junio</p>
+                <strong><p>Atracciones:</strong> Barcelona, Madrid, Sevilla</p>
             </div>
         </div>
         <div class="card">
@@ -89,26 +113,28 @@
             <h2>Japón</h2>
             <button onclick="this.nextElementSibling.style.display='block'">View Detail</button>
             <div class="details" style="display: none;">
-                <p>Descripción: Japón es conocido por su rica cultura y tecnología avanzada. Desde los templos
+                <strong><p>Descripción:</strong> Japón es conocido por su rica cultura y tecnología avanzada. Desde los templos
                     históricos y los hermosos cerezos en flor hasta los rascacielos modernos y la electrónica de
                     vanguardia, Japón ofrece una mezcla única de lo antiguo y lo nuevo.</p>
-                <p>Duración: 14 días</p>
-                <p>Mejor época: Marzo - Mayo</p>
-                <p>Atracciones: Tokio, Kioto, Hiroshima</p>
+                <strong><p>Duración:</strong> 14 días</p>
+                <strong><p>Mejor época:</strong> Marzo - Mayo</p>
+                <strong><p>Atracciones:</strong> Tokio, Kioto, Hiroshima</p>
             </div>
         </div>
+    </div>
+    <div class="container">
         <div class="card">
             <img src="https://www.thecommerce.es/WebRoot/StoreES3/Shops/ec4701/5263/F032/6D5D/ED65/0276/AC10/1416/8E58/Peru_m.png"
                 alt="Imagen de Perú">
             <h2>Perú</h2>
             <button onclick="this.nextElementSibling.style.display='block'">View Detail</button>
             <div class="details" style="display: none;">
-                <p>Descripción: Conocido por su rica historia y cultura, Perú es famoso por sus sitios arqueológicos,
+                <strong><p>Descripción:</strong> Conocido por su rica historia y cultura, Perú es famoso por sus sitios arqueológicos,
                     incluyendo Machu Picchu, una antigua ciudad inca en la Cordillera de los Andes. La capital, Lima,
                     alberga el Museo Larco, que alberga una colección de arte precolombino.</p>
-                <p>Duración: 7 días</p>
-                <p>Mejor época: Mayo - Septiembre</p>
-                <p>Atracciones: Machu Picchu, Cusco, Lago Titicaca</p>
+                <strong><p>Duración:</strong> 7 días</p>
+                <strong><p>Mejor época:</strong> Mayo - Septiembre</p>
+                <strong><p>Atracciones:</strong> Machu Picchu, Cusco, Lago Titicaca</p>
             </div>
         </div>
         <div class="card">
@@ -117,12 +143,12 @@
             <h2>Uruguay</h2>
             <button onclick="this.nextElementSibling.style.display='block'">View Detail</button>
             <div class="details" style="display: none;">
-                <p>Descripción: A menudo eclipsado por sus vecinos más grandes, Argentina y Brasil, Uruguay es un
+                <strong><p>Descripción:</strong> A menudo eclipsado por sus vecinos más grandes, Argentina y Brasil, Uruguay es un
                     destino turístico en crecimiento conocido por su tranquila costa, su animada capital, Montevideo, y
                     su próspera escena vinícola.</p>
-                <p>Duración: 5 días</p>
-                <p>Mejor época: Diciembre - Marzo</p>
-                <p>Atracciones: Montevideo, Punta del Este, Colonia del Sacramento</p>
+                <strong><p>Duración:</strong> 5 días</p>
+                <strong><p>Mejor época:</strong> Diciembre - Marzo</p>
+                <strong><p>Atracciones:</strong> Montevideo, Punta del Este, Colonia del Sacramento</p>
             </div>
         </div>
     </div>
